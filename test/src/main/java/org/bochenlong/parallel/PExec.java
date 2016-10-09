9935ff7851e140ab3c42fa30d55a02cbc8817a29
bochenlong <bochenlong@163.com>
2016-10-09 09:17:52 +0800

    add

--- test/src/main/java/org/bochenlong/parallel/PExec.java
+++ test/src/main/java/org/bochenlong/parallel/PExec.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Collectors;
 
 /**
  * Created by bochenlong on 16-10-8.
@@ -48,4 +49,5 @@ public class PExec {
         System.out.println("平均每条数据处理：" + (float) t1 / ac);
         executorService.shutdown();
     }
+
 }
